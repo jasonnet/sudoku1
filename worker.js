ec05ce447ddac58f60ca630a18cc737112b20e4f
Jason Crawford on SurfacePro8 <on_surfpro8@jasoncrawford.com>
2026-08-07 16:18:34 -0400

    refresh version number

--- worker.js
+++ worker.js
@@ -1,6 +1,6 @@
 'use strict';
 // Cache store name to save static resources
-const CACHE_STATIC = 'static-cache-v1';
+const CACHE_STATIC = 'static-cache-v2';
 // Minimal set of files to cache to be a PWA
 const FILES_TO_CACHE = [
     './',
