3084269f8f119af46224e816cc2f91d8ece87eb7
Jason Crawford on SurfacePro8 <on_surfpro8@jasoncrawford.com>
2026-08-07 23:01:13 -0400

    try again to improve formatting on the phone.

--- worker.js
+++ worker.js
@@ -1,6 +1,6 @@
 'use strict';
 // Cache store name to save static resources
-const CACHE_STATIC = 'static-cache-v3';
+const CACHE_STATIC = 'static-cache-v4';
 // Minimal set of files to cache to be a PWA
 const FILES_TO_CACHE = [
     './',
